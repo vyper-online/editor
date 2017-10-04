0ac7521790b4e55954e30a71d7f426cd44fa58da
holgerd77 <Holger.Drewes@gmail.com>
2017-10-04 15:34:49 +0200

    First version

--- js/api-1.0.0.js
+++ js/api-1.0.0.js
@@ -1,18 +1,33 @@
-var API_URL =  'https://api.viper.tools/'
-
-var COMPILE_ENDPOINT = 'compile/'
-
+const API_URL =  'https://api.viper.tools/'
+const COMPILE_ENDPOINT = 'compile/'
+const EXAMPLE_URL = 'https://raw.githubusercontent.com/ethereum/viper/master/examples/'
 
 
 var API = (function(API, $, undefined) {
+  
+  API.editor = null
   
   API.params = {
     'source': ""
   }
   
+  API.loadExample = function(example, showConfirm) {
+    var confirmed = true;
+    if (showConfirm) {
+      var loadMsg = "Current editor contents will be lost! Continue?";
+      confirmed = confirm(loadMsg);
+    }
+    
+    if (confirmed) {
+      $.get( EXAMPLE_URL + example, function( data ) {
+        API.editor.setValue(data);
+      });
+    }
+  }
+  
   API.load = function() {
     $('#result').empty();
-    this.params['source'] = $('#viperSourceCode').val();
+    this.params['source'] = this.editor.getValue();
     
     $.ajax({
       url: API_URL + COMPILE_ENDPOINT,
@@ -21,9 +36,19 @@ var API = (function(API, $, undefined) {
       dataType: 'json',
       data: this.params,
       success: function(data) {
-        $('#result').html(data.result.bytecode)
+        if (data.result.bytecode_code === 200) {
+          $('#bytecodeResult').html('<i class="fa fa-check" aria-hidden="true"></i>')
+        } else {
+          $('#bytecodeResult').html('<i class="fa fa-exclamation-circle" aria-hidden="true"></i>')
+        }
+        $('#bytecode').html(data.result.bytecode)
         
-        $('#result2').html(data.result.json)
+        if (data.result.json_code === 200) {
+          $('#abiResult').html('<i class="fa fa-check" aria-hidden="true"></i>')
+        } else {
+          $('#abiResult').html('<i class="fa fa-exclamation-circle" aria-hidden="true"></i>')
+        }
+        $('#abi').html(data.result.json)
       },
       fail: function() {
         $('#result').html("Mah.")
@@ -33,6 +58,25 @@ var API = (function(API, $, undefined) {
   
   API.init = function() {
     console.log("API initialized.")
+    
+    this.editor = ace.edit("editor");
+    this.editor.setTheme("ace/theme/crimson_editor");
+    this.editor.getSession().setMode("ace/mode/python"); 
+    
+    if (localStorage.getItem("currentDocument")) {
+      this.editor.setValue(localStorage.getItem("currentDocument"));
+    } else {
+      API.loadExample('voting/ballot.v.py', false);
+    }
+    
+    
+    this.editor.getSession().on('change', function(e) {
+      if (true) {
+        localStorage.setItem("currentDocument", API.editor.getValue());
+        this.changeCounter = 0;
+      }
+      this.changeCounter += 1;
+    });
   }
   
   return API;
