a2fba6e2e358e638997936660eb254621fe4bf22
Ben Kaufman <ben.kaufman10@gmail.com>
2018-03-30 09:16:19 +0300

    Added Desktop Version Using Electron
    Initialized npm and installed electron.js package.
    Added the main.js to create the electron desktop app.
    Added desktop icons.
    Updated the README.md and added it a download link to the desktop version.

---
+++ main.js
@@ -0,0 +1,217 @@
+	const {
+		app,
+		Menu,
+		BrowserWindow
+	} = require('electron')
+	const path = require('path')
+	const url = require('url')
+
+	// Keep a global reference of the window object, if you don't, the window will
+	// be closed automatically when the JavaScript object is garbage collected.
+	let win
+
+	function createWindow() {
+		// Create the browser window.
+		win = new BrowserWindow({
+			width: 800,
+			height: 600
+		})
+
+		// and load the index.html of the app.
+		win.loadURL(url.format({
+			pathname: path.join(__dirname, 'index.html'),
+			protocol: 'file:',
+			slashes: true
+		}))
+
+		// Emitted when the window is closed.
+		win.on('closed', () => {
+			// Dereference the window object, usually you would store windows
+			// in an array if your app supports multi windows, this is the time
+			// when you should delete the corresponding element.
+			win = null
+		})
+
+		const {
+			app,
+			Menu
+		} = require('electron')
+
+		const template = [{
+				label: 'Edit',
+				submenu: [{
+						role: 'undo'
+					},
+					{
+						role: 'redo'
+					},
+					{
+						type: 'separator'
+					},
+					{
+						role: 'cut'
+					},
+					{
+						role: 'copy'
+					},
+					{
+						role: 'paste'
+					},
+					{
+						role: 'pasteandmatchstyle'
+					},
+					{
+						role: 'delete'
+					},
+					{
+						role: 'selectall'
+					}
+				]
+			},
+			{
+				label: 'View',
+				submenu: [{
+						role: 'reload'
+					},
+					{
+						role: 'forcereload'
+					},
+					{
+						role: 'toggledevtools'
+					},
+					{
+						type: 'separator'
+					},
+					{
+						role: 'resetzoom'
+					},
+					{
+						role: 'zoomin'
+					},
+					{
+						role: 'zoomout'
+					},
+					{
+						type: 'separator'
+					},
+					{
+						role: 'togglefullscreen'
+					}
+				]
+			},
+			{
+				role: 'window',
+				submenu: [{
+						role: 'minimize'
+					},
+					{
+						role: 'close'
+					}
+				]
+			},
+			{
+				role: 'help',
+				submenu: [{
+					label: 'Learn More',
+					click() {
+						require('electron').shell.openExternal('https://electronjs.org')
+					}
+				}]
+			}
+		]
+
+		if (process.platform === 'darwin') {
+			template.unshift({
+				label: app.getName(),
+				submenu: [{
+						role: 'about'
+					},
+					{
+						type: 'separator'
+					},
+					{
+						role: 'services',
+						submenu: []
+					},
+					{
+						type: 'separator'
+					},
+					{
+						role: 'hide'
+					},
+					{
+						role: 'hideothers'
+					},
+					{
+						role: 'unhide'
+					},
+					{
+						type: 'separator'
+					},
+					{
+						role: 'quit'
+					}
+				]
+			})
+
+			// Edit menu
+			template[1].submenu.push({
+				type: 'separator'
+			}, {
+				label: 'Speech',
+				submenu: [{
+						role: 'startspeaking'
+					},
+					{
+						role: 'stopspeaking'
+					}
+				]
+			})
+
+			// Window menu
+			template[3].submenu = [{
+					role: 'close'
+				},
+				{
+					role: 'minimize'
+				},
+				{
+					role: 'zoom'
+				},
+				{
+					type: 'separator'
+				},
+				{
+					role: 'front'
+				}
+			]
+		}
+
+		const menu = Menu.buildFromTemplate(template)
+		Menu.setApplicationMenu(menu)
+	}
+
+	// This method will be called when Electron has finished
+	// initialization and is ready to create browser windows.
+	// Some APIs can only be used after this event occurs.
+	app.on('ready', createWindow)
+
+	// Quit when all windows are closed.
+	app.on('window-all-closed', () => {
+		// On macOS it is common for applications and their menu bar
+		// to stay active until the user quits explicitly with Cmd + Q
+		if (process.platform !== 'darwin') {
+			app.quit()
+		}
+	})
+
+	app.on('activate', () => {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (win === null) {
+			createWindow()
+		}
+	})
+
+	// In this file you can include the rest of your app's specific main process
+	// code. You can also put them in separate files and require them here.
